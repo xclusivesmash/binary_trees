7b6cab0c8a371f7ae5b037cdbc3fdb5545b1b778
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 14:27:29 -0700

    Updated code

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,8 +3,17 @@
 /**
 * binary_tree_height - measures the height of tree.
 * @tree: binary tree given.
-* Return; size of @tree. Otherwise, 0.
+* Return: size of @tree. Otherwise, 0.
 */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t L, R;
+	/* BASE CASE */
+	if (tree == NULL)
+		return (0);
+	L = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	R = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	if (L > R)
+		return (L);
+	return (R);
 }
