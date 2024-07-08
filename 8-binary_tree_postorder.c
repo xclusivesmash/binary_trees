be4f1a9ee8b074c8cd80cf0926cfed7b75e1bf04
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 14:12:37 -0700

    Updated code

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -8,4 +8,10 @@
 */
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 {
+	/* BASE CASE */
+	if (tree == NULL || func == NULL)
+		return;
+	binary_tree_postorder(tree->left, func);
+	binary_tree_postorder(tree->right, func);
+	(*func)(tree->n);
 }
