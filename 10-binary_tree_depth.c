0ac4c14244488274a96c028f61f0198f7ca5ac29
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 14:52:03 -0700

    Updated code

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,4 +8,16 @@
 */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	size_t depth;
+
+	depth = 0;
+	/* INPUT CHECK */
+	if (tree == NULL)
+		return (0);
+	while (tree->parent != NULL)
+	{
+		depth = depth + 1;
+		tree = tree->parent;
+	}
+	return (depth);
 }
