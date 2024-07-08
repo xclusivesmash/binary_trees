d2b59208335805b408820b3268ad561eabe8ded7
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 13:15:23 -0700

    Updated code

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -7,4 +7,10 @@
 */
 void binary_tree_delete(binary_tree_t *tree)
 {
+	/* INPUT CHECK */
+	if (tree == NULL)
+		return;
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
