c53e3a3f959c91ae1bd5486990bf6b1c412a43e1
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 13:03:01 -0700

    Updated binary_tree_insert_left function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -5,8 +5,23 @@
 * to an existing node.
 * @parent: root node.
 * @value: value to insert.
-* Rerturn: Created node or NULL upon failure.
+* Return: Created node or NULL upon failure.
 */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+	/* INPUT CHECK */
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->left = parent->left;
+	new->right = NULL;
+	parent->left = new;
+	if (new->left != NULL)
+		new->left->parent = new;
+	return (new);
 }
