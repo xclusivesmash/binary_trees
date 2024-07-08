cabae3bb71ccf947a12f2bf8fd54cd9857acd73e
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 13:43:40 -0700

    Updated code

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,4 +7,10 @@
 */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+	/* INPUT CHECK */
+	if (node == NULL)
+		return (0);
+	if (node->left == NULL && node->right == NULL)
+		return (1);
+	return (0);
 }
