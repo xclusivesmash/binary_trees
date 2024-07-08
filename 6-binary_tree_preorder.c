cd4b3e7e7ec5e0988d55078290ea986e029042e7
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 14:04:50 -0700

    Updated code

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -8,4 +8,10 @@
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
+	/* INPUT CHECK */
+	if (tree == NULL || func == NULL)
+		return;
+	(*func)(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
