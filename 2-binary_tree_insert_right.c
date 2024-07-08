56b4bbc960052e1711e25341551b68f1c5ac9a51
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 13:06:50 -0700

    Updated code base

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,4 +9,19 @@
 */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
+	/* INPUT CHECK */
+	if (parent == NULL)
+		return (NULL);
+	new = malloc(sizeof(binary_tree_t));
+	if (new == NULL)
+		return (NULL);
+	new->parent = parent;
+	new->n = value;
+	new->right = parent->right;
+	new->left = NULL;
+	parent->right = new;
+	if (new->right != NULL)
+		new->right->parent = new;
+	return (new);
 }
