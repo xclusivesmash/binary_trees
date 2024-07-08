fee52d5a1af052d003835b77e4bf3cbfda291375
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-08 14:57:50 -0700

    Updated code

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,4 +7,8 @@
 */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
+	/* BASE CASE */
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
