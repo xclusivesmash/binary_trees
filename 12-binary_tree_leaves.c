d5cc6f558add53396d995f45eb015af25385d554
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-09 13:14:59 -0700

    Updated code

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,4 +7,10 @@
 */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	/* INPUT CHECK */
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
 }
