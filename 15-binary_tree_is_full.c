f1a1987c864f7639996c256098e642c2596f207c
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-09 13:47:35 -0700

    Updated code

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -7,4 +7,13 @@
 */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	/* INPUT CHECK */
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) &&
+			binary_tree_is_full(tree->right));
+	return (0);
 }
