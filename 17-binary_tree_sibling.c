c4fff435316e683031817e1f90e94d654fc345d1
xclusivesmash <sbumatshiane916@gmail.com>
2024-07-09 14:16:33 -0700

    Updated code

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -7,4 +7,10 @@
 */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
+	/* INPUT CHECK */
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node != node->parent->left)
+		return (node->parent->left);
+	return (node->parent->right);
 }
